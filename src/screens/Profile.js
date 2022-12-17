e2293a457ffc163c34b344a10d887987ca709385
annasantony612 <annasantony612@gmail.com>
2022-12-18 04:22:00 +0530

    history,profile pages created

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -6,7 +6,34 @@ const Profile = ({navigation}) => {
   const {signOut} = React.useContext(AuthContext);
   return (
     <View style={{flex: 1}}>
-      <View style={{flex: 4}}>{/* <Atmcard /> */}</View>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: '#02C3BD',
+          borderBottomLeftRadius: 40,
+          borderBottomRightRadius: 40,
+        }}>
+        <Text
+          style={{
+            color: '#FFFFFF',
+            fontSize: 40,
+            fontWeight: '700',
+            paddingBottom: 10,
+            paddingLeft: 20,
+          }}>
+          Shaji A C
+        </Text>
+        <Text
+          style={{
+            color: '#FFFFFF',
+            fontSize: 18,
+            fontWeight: '700',
+            paddingLeft: 20,
+          }}>
+          shaji@gmail.com
+        </Text>
+      </View>
+      <View style={{flex: 2}}></View>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Button onPress={() => signOut()} title="Sign out" />
       </View>
--- src/screens/Start.js
+++ src/screens/Start.js
@@ -1,59 +1,67 @@
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
 import React, {useContext} from 'react';
 import Button from '../components/Button';
 import {TextInput} from 'react-native-gesture-handler';
 import {AuthContext} from '../Context';
+import AtmCard from '../../assets/images/AtmCard.png';
+import LoginButton from '../components/LoginButton';
 const Start = () => {
   const {signIn} = useContext(AuthContext);
   return (
     <View style={{flex: 1}}>
-      <View
-        style={{
-          flex: 3,
-          paddingLeft: 30,
-          alignContent: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={{paddingBottom: 10, paddingTop: 30, color: 'black'}}>
-          Name
-        </Text>
-        <TextInput
+      <View style={{flex: 3}}>
+        <ImageBackground
+          source={AtmCard}
+          // resizeMode="cover"
           style={{
-            width: 300,
-            height: 41,
-            backgroundColor: '#D9D9D9',
-            borderRadius: 10,
-            color: 'black',
-          }}></TextInput>
-        <Text style={{paddingBottom: 10, paddingTop: 30, color: 'black'}}>
-          Email ID
-        </Text>
-
-        <TextInput
-          style={{
-            width: 300,
-            height: 41,
-            backgroundColor: '#D9D9D9',
-            borderRadius: 10,
-            color: 'black',
-          }}></TextInput>
-        <Text style={{paddingBottom: 10, paddingTop: 30, color: 'black'}}>
-          Mobile Number
-        </Text>
-
-        <TextInput
-          style={{
-            width: 300,
-            height: 41,
-            backgroundColor: '#D9D9D9',
-            borderRadius: 10,
-            color: 'black',
-          }}
-          onChangeText={e => setPhone(e)}></TextInput>
+            height: 200,
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}></ImageBackground>
       </View>
 
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Button onPress={() => signIn()} title="Login" />
+      <View style={{flex: 4}}>
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{color: 'black', fontSize: 24, fontWeight: '600'}}>
+            Create An Account
+          </Text>
+        </View>
+
+        <View
+          style={{
+            paddingLeft: 30,
+            alignContent: 'center',
+            justifyContent: 'center',
+            paddingTop: 20,
+          }}>
+          <Text style={{paddingBottom: 10, color: 'black'}}>Name</Text>
+          <TextInput
+            style={{
+              width: 300,
+              height: 41,
+              backgroundColor: '#D9D9D9',
+              borderRadius: 10,
+              color: 'black',
+            }}></TextInput>
+          <Text style={{paddingBottom: 10, paddingTop: 30, color: 'black'}}>
+            Email ID
+          </Text>
+
+          <TextInput
+            style={{
+              width: 300,
+              height: 41,
+              backgroundColor: '#D9D9D9',
+              borderRadius: 10,
+              color: 'black',
+            }}></TextInput>
+        </View>
+      </View>
+
+      <View style={{flex: 2, alignItems: 'center'}}>
+        <LoginButton onPress={() => signIn()} title="Sign up" />
       </View>
     </View>
   );
