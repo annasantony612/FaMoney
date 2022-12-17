9fc3af840c7d83a9e012295e8a038b9a97e82a7d
annasantony612 <annasantony612@gmail.com>
2022-12-17 19:49:11 +0530

    navigation set

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,43 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import Button from '../components/Button';
+import CategoryPg from './CategoryPg';
+import Login from './Liability';
 
-const Home = () => {
+const Home = ({navigation}) => {
+  console.log(navigation);
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to FaMoney</Text>
+      </View>
+      <View style={styles.container1}>
+        <Button
+          onPress={() => navigation.navigate('CategoryPg')}
+          navigation={navigation}
+          title="Add Expenses"
+        />
+      </View>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 13,
+  },
+  container1: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 30,
+  },
+});
