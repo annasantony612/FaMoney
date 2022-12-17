787d101dd23a55160bea4e3fc6e5c379e716c326
Aiswarya <aiswaryatp73@gmail.com>
2022-12-17 23:01:25 +0530

    bills added

---
+++ src/screens/Bills.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+const Liability = () => {
+  return (
+    <View style={Styles.container}>
+      <View style={Styles.container1}>
+        <Text style={Styles.text1}> 🧾 Bills</Text>
+      </View>
+      <View style={Styles.container2}>
+        <Text style={Styles.text2}> Name</Text>
+        <TextInput
+          style={{
+            width: 300,
+            height: 41,
+            backgroundColor: '#D9D9D9',
+            borderRadius: 10,
+          }}></TextInput>
+
+        <Text style={Styles.text2}>Installment Amount</Text>
+        <TextInput
+          style={{
+            width: 300,
+            height: 41,
+            backgroundColor: '#D9D9D9',
+            borderRadius: 10,
+          }}></TextInput>
+      </View>
+    </View>
+  );
+};
+
+export default Liability;
+Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  container1: {
+    flex: 1,
+    padding: 10,
+  },
+  container2: {
+    flex: 4,
+    paddingLeft: 30,
+  },
+  text1: {
+    color: 'black',
+    fontSize: 28,
+    fontWeight: '400',
+    textAlign: 'center',
+    marginTop: 30,
+  },
+  text2: {
+    padding: 10,
+    fontSize: 15,
+    color: 'black',
+  },
+  textbox: {
+    width: 300,
+    height: 41,
+    borderRadius: 10,
+    backgroundColor: '#D9D9D9',
+  },
+});
